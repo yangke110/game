2ef0074bb8e50a51d62db98174474e17406d6d8b
yangke110 <yangke1110@gmail.com>
2023-05-06 12:15:34 +0800

    add settings

--- enterence/start.py
+++ enterence/start.py
@@ -1,10 +1,8 @@
-import time
-
 import keyboard
 from logic import logic_main
 
 if __name__ == '__main__':
-    print('Qn Auto Script ,Create By  wadJohnny Yang!\n')
+    print('Qn Auto Script ,Create By  Johnny Yang!\n')
     print('Press F10 to Start...Press F12 to End...')
     while True:
         if keyboard.is_pressed('F10'):
